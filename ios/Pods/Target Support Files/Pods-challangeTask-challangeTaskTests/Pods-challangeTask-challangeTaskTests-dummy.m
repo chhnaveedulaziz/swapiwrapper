c2bd8b90b38ebfdbb802bd581c1257a36e221525
theherd8@invozone.com <codefic@invozone.com>
2022-04-01 20:51:01 +0500

    Project setup Keeping check on code quality.

---
+++ ios/Pods/Target Support Files/Pods-challangeTask-challangeTaskTests/Pods-challangeTask-challangeTaskTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_challangeTask_challangeTaskTests : NSObject
+@end
+@implementation PodsDummy_Pods_challangeTask_challangeTaskTests
+@end
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+const App=()=>{
+    return(
+        <View>
+            <Text>{"Project setup"}</Text>
+        </View>
+    )
+}
+export default App;
