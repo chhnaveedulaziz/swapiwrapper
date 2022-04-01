af938ec52bdcc2ee29ee09207b1ac7022265b516
theherd8@invozone.com <codefic@invozone.com>
2022-04-01 21:05:36 +0500

    Fetching Data from API,Setting loaders properly and prettifying UX

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from "react";
-import { View, Text } from "react-native";
-const App=()=>{
-    return(
-        <View>
-            <Text>{"Project setup"}</Text>
-        </View>
-    )
+import React, { useEffect } from 'react';
+import {
+    ScrollView,
+    View
+} from 'react-native';
+import { styles } from "./App.style";
+import { useApi } from "./Hooks/useApi";
+import constant from "./Utils/ApiConstants";
+import Loader from "./Utils/Loader";
+const App = () => {
+    let text
+    useEffect(() => { apiCall(constant.Base_url + "people/1/") }, [])
+    const [isLoading, data, error, apiCall] = useApi();
+   
+    if (isLoading) return <Loader visible={isLoading} />
+    return (
+        <ScrollView
+            contentInsetAdjustmentBehavior="automatic"
+            style={styles.scrollView}>
+            <View style={styles.container}>
+                <Text>{"Api call"}</Text>
+            </View>
+        </ScrollView>
+    );
 }
 export default App;
